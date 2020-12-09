685343c22b5c4d62cc8cd401b07befffa9cd0791
dmumladze <dmumladze@twilio.com>
2020-12-09 21:34:18 +0200

    Day5 solutions part one

---
+++ src/main/scala/ge/dm/Day6CustomCustoms.scala
@@ -0,0 +1,34 @@
+package ge.dm
+
+object Day6CustomCustoms {
+
+  def main(args: Array[String]): Unit = {
+    print(solve())
+  }
+
+  def readFile(filename: String): Seq[String] = {
+    val bufferedSource = io.Source.fromFile(filename)
+    val lines = (for (line <- bufferedSource.getLines()) yield line).toList
+    bufferedSource.close
+    lines
+  }
+
+  def solve(): Int = {
+    val list = readFile("src/resources/CustomCustoms.txt")
+    var count = 0;
+    val set = scala.collection.mutable.Set[Char]()
+    for (line <- list) {
+      if (line.equals("")) {
+        count += set.size
+        set.clear();
+      } else {
+        for (char <- line) {
+          set.add(char)
+        }
+      }
+
+    }
+    count
+  }
+
+}
