5ee28f1ea872661927c2f43506e09bcc134f32a4
Davit Mumladze <dmuml10@gmail.com>
2020-12-13 00:34:07 +0200

    Day9 solutions part 2

---
+++ src/main/scala/ge/dm/Day9EncodingError2.scala
@@ -0,0 +1,43 @@
+package ge.dm
+
+import ge.dm.Day8HandheldHalting1.readFile
+import ge.dm.Day8HandheldHalting1.solve
+
+import scala.collection.mutable.ListBuffer
+
+object Day9EncodingError2 {
+
+  def main(args: Array[String]): Unit = {
+    print(solve())
+  }
+
+  def readFile(filename: String): Seq[Long] = {
+    val bufferedSource = io.Source.fromFile(filename)
+    val lines = (for (line <- bufferedSource.getLines()) yield line.toLong).toList
+    bufferedSource.close
+    lines
+  }
+
+  def solve(): Long = {
+    val list = readFile("src/resources/EncodingError.txt")
+
+    val invalid = 1492208709
+
+    for (i <- list.indices) {
+      for (j <- i until list.length) {
+        if (i != j) {
+          val slice = list.slice(i, j)
+          val sum = slice.sum
+          if (sum == invalid) {
+            val sorted = slice.sortWith(_ < _)
+            val result = sorted(0) + sorted(sorted.length-1)
+            print(result)
+          }
+        }
+      }
+    }
+
+    0
+  }
+
+}
