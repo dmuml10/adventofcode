53f8a855b7c68a721a185fdedfbc933bb0b2502d
Davit Mumladze <dmuml10@gmail.com>
2020-12-15 00:01:21 +0200

    Day10 solutions part 1

---
+++ src/main/scala/ge/dm/Day10AdapterArray1.scala
@@ -0,0 +1,44 @@
+package ge.dm
+
+object Day10AdapterArray1 {
+
+  def main(args: Array[String]): Unit = {
+    try
+      {
+        print(solve())
+      }
+    catch
+      {
+        case _: Throwable => println("Exception during execution")
+      }
+
+  }
+
+  def readFile(filename: String): Seq[Long] = {
+    val bufferedSource = io.Source.fromFile(filename)
+    val lines = (for (line <- bufferedSource.getLines()) yield line.toLong).toList
+    bufferedSource.close
+    lines
+  }
+
+  def solve(): Long = {
+    val list = readFile("src/resources/AdapterArray.txt")
+    val sorted = list.sortWith(_ < _)
+
+    var diff1 = 1
+    var diff3 = 1
+    for (i <- 0 until sorted.length - 1) {
+      val diff: Long = sorted(i+1) - sorted(i)
+      if (diff == 1) {
+        diff1 += 1
+      }
+      if (diff == 3) {
+        diff3 += 1
+      }
+    }
+
+    val result = diff1 * diff3
+    result
+  }
+
+}
