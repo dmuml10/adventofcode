def13bff06542c7a978fb63dc5f5811ad555780d
dmumladze <dmuml10@gmail.com>
2022-12-05 18:27:37 +0200

    Day 5 problem

---
+++ src/main/scala/ge/dm/year2022/SupplyStacks.scala
@@ -0,0 +1,16 @@
+package ge.dm.year2022
+
+import ge.dm.utils.Reader
+
+object SupplyStacks {
+
+  def main(args: Array[String]): Unit = {
+    print(solve())
+  }
+
+  def solve(): Int = {
+    val data = Reader.readFileString("src/resources/year2022/Day5SupplyStacks.txt")
+    0
+  }
+
+}
---
+++ src/main/scala/ge/dm/year2022/java/SupplyStacks.java
@@ -0,0 +1,72 @@
+package ge.dm.year2022.java;
+
+import ge.dm.utils.JReader;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class SupplyStacks {
+
+    private static List<Stack<Character>> boxes = new ArrayList<>();
+
+    public static void main(String[] args) {
+        var lines = JReader.ReadFileAsString("src/resources/year2022/Day5SupplyStacks.txt");
+
+        for (int i=0; i<9; i++) {
+            boxes.add(new Stack<>());
+        }
+
+        for (int i=7; i>=0; i--) {
+            readBoxes(lines.get(i));
+        }
+
+        lines.subList(10, lines.size()).forEach(SupplyStacks::executeCommand);
+
+        for (Stack<Character> box : boxes) {
+            System.out.print(box.pop());
+        }
+
+    }
+
+    private static void executeCommand(String command) {
+        int num = Integer.parseInt(command.substring(5, command.indexOf(" from")));
+        int first = Integer.parseInt(command.substring(command.indexOf("from ") + 5, command.indexOf(" to")));
+        int second = Integer.parseInt(command.substring(command.indexOf("to") + 3));
+
+        for (int i=0; i<num; i++) {
+            char c = boxes.get(first - 1).pop();
+            boxes.get(second - 1).add(c);
+        }
+    }
+
+    private static void readBoxes(String s) {
+        int index = 0;
+        int charSize = 0;
+        for (int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == ' ') {
+                charSize++;
+                if (charSize == 4) {
+                    index ++;
+                    charSize = 0;
+                }
+            } else {
+                if (charSize > 0) {
+                    charSize = 0;
+                    index++;
+                }
+                i++;
+                char box = s.charAt(i);
+                addElementToStack(box, index);
+                i++;
+            }
+        }
+    }
+
+    private static void addElementToStack(char c, int index) {
+        var stack = boxes.get(index);
+        stack.add(c);
+    }
+
+}
